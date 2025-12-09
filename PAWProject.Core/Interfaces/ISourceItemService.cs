d82050f15d7dea0c8d5b154e8720eb2a499213b1
johel360CR <munozjohel369@gmail.com>
2025-12-08 18:19:56 -0600

    carga de de JSON y guardar datos en la BD

--- PAWProject.Core/Interfaces/ISourceItemService.cs
+++ PAWProject.Core/Interfaces/ISourceItemService.cs
@@ -1,16 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 using PAWProject.Data.Models;
-
 
 namespace PAWProject.Core.Interfaces
 {
     public interface ISourceItemService
     {
+        /// <summary>
+        /// Guarda un SourceItem en la base de datos.
+        /// </summary>
         Task<bool> SaveItemAsync(SourceItem item);
+
+        /// <summary>
+        /// Devuelve todos los SourceItems ordenados por fecha.
+        /// </summary>
+        Task<IEnumerable<SourceItem>> GetAllAsync();
+
+        /// <summary>
+        /// Devuelve un SourceItem por su Id, o null si no existe.
+        /// </summary>
+        Task<SourceItem?> GetByIdAsync(int id);
     }
 }
